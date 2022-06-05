f4e7b3d61c606db8dce743646ef2c5ffac9b5039
enzocazenave <encaze24@hotmail.com>
2022-06-05 01:11:28 -0300

    Add script name change control

--- server.lua
+++ server.lua
@@ -1,3 +1,8 @@
 ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+if GetCurrentResourceName() != "en_chips" then
+    print("Script turning off...")
+    StopResource()
+end
